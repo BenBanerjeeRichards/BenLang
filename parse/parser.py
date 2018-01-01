7fc3d716a388e0f14eec71d90669baadff7dea52
Ben Banerjee-Richards <dev@benbr.com>
2018-01-01 18:46:57 +0000

    Refactored

--- parse/parser.py
+++ parse/parser.py
@@ -1,10 +1,10 @@
 from antlr4 import *
 from BenLangLexer import BenLangLexer
-from BenLangVisitor import BenLangVisitor
 from BenLangParser import BenLangParser
 import sys
-import re
 from parse.ast import *
+from util.graph import draw_ast, draw_syntax_tree
+
 
 def is_terminal(x):
     return isinstance(x, tree.Tree.TerminalNodeImpl)
@@ -41,7 +41,7 @@ def to_ast(root):
         return StatementBlockNode(statements, start_position, stop_position)
 
     if isinstance(root, BenLangParser.StatementContext):
-        return to_ast(root.children[0])    # Ignore semicolon if exists
+        return to_ast(root.children[0])  # Ignore semicolon if exists
 
     if isinstance(root, BenLangParser.WhileLoopContext):
         assert len(root.children) == 7
@@ -78,8 +78,7 @@ def to_ast(root):
         return AssignmentNode(identifier, rhs, start_position, stop_position)
 
 
-
-def expression_to_ast(root : BenLangParser.StatementContext):
+def expression_to_ast(root: BenLangParser.StatementContext):
     if is_terminal(root):
         # Values
         return get_expression_value(root)
@@ -183,68 +182,6 @@ def get_expression_binary_ast(operator, lhs, rhs, start_position, stop_position)
         return OpLessThanNode(lhs, rhs, start_position, stop_position)
 
 
-def graphviz(t, is_root_node, node_text, get_children, relations=[], labels={}, node_key=0):
-    child_key = node_key
-
-    labels[node_key] = sanitize_graphviz_label(node_text(t))
-
-    if is_root_node(t):
-        return node_key, relations, labels
-
-    for child in get_children(t):
-        child_key += 1
-        relations.append((node_key, child_key))
-        child_key += graphviz(child, is_root_node, node_text, get_children,  relations, labels, child_key)[0]
-
-    return child_key, relations, labels
-
-
-def graphviz_output(relations, labels):
-    out = ""
-    out += "digraph g {\n"
-    for relation in relations:
-        out += "\t{} -> {}\n".format(relation[0], relation[1])
-    for k, v in labels.items():
-        out += "\t{}[label=\"{}\"]\n".format(k, v)
-    out += "}\n"
-
-    return out
-
-
-def ctx_text(ctx, symbolic_names):
-    if isinstance(ctx, tree.Tree.TerminalNodeImpl):
-        pattern = re.compile(r'\s+')
-        text = re.sub(pattern, '', ctx.symbol.text)
-
-        if len(text) > 0:
-            return "{}({})".format(symbolic_names[ctx.symbol.type], text)
-        else:
-            return "{}".format(text)
-
-    else:
-        return ctx.__class__.__name__.replace("Context", "")
-
-
-def sanitize_graphviz_label(text):
-    return text.replace("\"", "\\\"")
-
-
-def draw_syntax_tree(theTree, symbolic_names):
-    out = graphviz(theTree, lambda t: isinstance(t, tree.Tree.TerminalNodeImpl), lambda x: ctx_text(x, symbolic_names), lambda x: x.getChildren())
-    write_gv_output(out)
-
-
-def draw_ast(ast : Node):
-    out = graphviz(ast, lambda t: len(ast.get_children()) == 0, lambda x: x.__str__(), lambda x : x.get_children())
-    write_gv_output(out)
-
-
-def write_gv_output(output):
-    gv = graphviz_output(output[1], output[2])
-    f = open("graphviz", "w+")
-    f.write(gv)
-    f.close()
-
 def main(argv):
     lexer = BenLangLexer(FileStream(argv[1]))
     stream = CommonTokenStream(lexer)
@@ -252,8 +189,6 @@ def main(argv):
     tree = parser.prog()
     x = to_ast(tree)
     draw_ast(x)
-
-    #draw_syntax_tree(tree, BenLangLexer.symbolicNames)
 
 
 if __name__ == '__main__':
---
+++ util/graph.py
@@ -0,0 +1,65 @@
+from parse.ast import Node
+import re
+
+
+def graphviz(t, is_root_node, node_text, get_children, relations=[], labels={}, node_key=0):
+    child_key = node_key
+
+    labels[node_key] = sanitize_graphviz_label(node_text(t))
+
+    if is_root_node(t):
+        return node_key, relations, labels
+
+    for child in get_children(t):
+        child_key += 1
+        relations.append((node_key, child_key))
+        child_key += graphviz(child, is_root_node, node_text, get_children,  relations, labels, child_key)[0]
+
+    return child_key, relations, labels
+
+
+def graphviz_output(relations, labels):
+    out = ""
+    out += "digraph g {\n"
+    for relation in relations:
+        out += "\t{} -> {}\n".format(relation[0], relation[1])
+    for k, v in labels.items():
+        out += "\t{}[label=\"{}\"]\n".format(k, v)
+    out += "}\n"
+
+    return out
+
+
+def ctx_text(ctx, symbolic_names):
+    if isinstance(ctx, tree.Tree.TerminalNodeImpl):
+        pattern = re.compile(r'\s+')
+        text = re.sub(pattern, '', ctx.symbol.text)
+
+        if len(text) > 0:
+            return "{}({})".format(symbolic_names[ctx.symbol.type], text)
+        else:
+            return "{}".format(text)
+
+    else:
+        return ctx.__class__.__name__.replace("Context", "")
+
+
+def sanitize_graphviz_label(text):
+    return text.replace("\"", "\\\"")
+
+
+def draw_syntax_tree(theTree, symbolic_names):
+    out = graphviz(theTree, lambda t: isinstance(t, tree.Tree.TerminalNodeImpl), lambda x: ctx_text(x, symbolic_names), lambda x: x.getChildren())
+    write_gv_output(out)
+
+
+def draw_ast(ast : Node):
+    out = graphviz(ast, lambda t: len(ast.get_children()) == 0, lambda x: x.__str__(), lambda x : x.get_children())
+    write_gv_output(out)
+
+
+def write_gv_output(output):
+    gv = graphviz_output(output[1], output[2])
+    f = open("graphviz", "w+")
+    f.write(gv)
+    f.close()
